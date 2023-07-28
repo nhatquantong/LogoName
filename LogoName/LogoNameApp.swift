f9117a99e2961f73b254ece84ef1841a34814778
nhatquan0312 <nhatquan0312@gmail.com>
2023-07-28 15:07:45 +0700

    Initial Commit

---
+++ LogoName/LogoNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogoNameApp.swift
+//  LogoName
+//
+//  Created by Quan Tong Nhat on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LogoNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
